b035911756f8b4f0d3d07d82f94465177fb6c297
superbeom <it.super.beom@gmail.com>
2020-11-18 09:19:32 +0900

    Update StartButton - Animation - Easing.bounce

--- src/components/StartButton.js
+++ src/components/StartButton.js
@@ -1,20 +1,65 @@
-import React from "react";
-import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import React, { useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Animated,
+  Easing,
+} from "react-native";
 import { vw } from "react-native-expo-viewport-units";
 
 import colors from "../constants/colors";
 import { GAME_START, UPDATING } from "../constants/strings";
 
+const size = vw(40);
+const startRange = vw(30);
+
 const StartButton = ({ onPress, update, enoughHeart }) => {
+  let opacity = new Animated.Value(0.7);
+
+  const zeroToOne = () => {
+    opacity.setValue(0.7);
+    return Animated.timing(opacity, {
+      toValue: 1,
+      duration: 2000,
+      easing: Easing.bounce,
+      useNativeDriver: false,
+    });
+  };
+
+  const oneToZero = () => {
+    opacity.setValue(1);
+    return Animated.timing(opacity, {
+      toValue: 0.7,
+      duration: 1500,
+      delay: 3000,
+      useNativeDriver: false,
+    });
+  };
+
+  const executeAnimation = () => {
+    Animated.loop(Animated.sequence([zeroToOne(), oneToZero()])).start();
+  };
+
+  const measure = opacity.interpolate({
+    inputRange: [0, 1],
+    outputRange: [startRange, size],
+  });
+
+  useEffect(() => {
+    executeAnimation();
+  }, []);
+
   return (
     <TouchableOpacity
       onPress={onPress}
       activeOpacity={0.5}
       disabled={update || !enoughHeart}
     >
-      <View
+      <Animated.View
         style={[
           styles.button,
+          { opacity, width: measure, height: measure },
           !enoughHeart
             ? {
                 backgroundColor: colors.grayColor,
@@ -40,16 +85,14 @@ const StartButton = ({ onPress, update, enoughHeart }) => {
         >
           {update ? UPDATING : GAME_START}
         </Text>
-      </View>
+      </Animated.View>
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
   button: {
-    width: vw(40),
-    height: vw(40),
-    borderRadius: vw(40) / 2,
+    borderRadius: size / 2,
     justifyContent: "center",
     alignItems: "center",
     shadowOffset: { width: 0, height: 2 },
