8af1f80b4fee91bd6581f2314fcd4babb2f36bc1
superbeom <it.super.beom@gmail.com>
2020-11-24 11:42:49 +0900

    Preloading Assets

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Asset } from "expo-asset";
 
 import { GameProvider } from "./src/context/GameContext";
 import AppStack from "./src/stacks/AppStack";
 
-export default () => (
-  <GameProvider>
-    <AppStack />
-  </GameProvider>
-);
+import Loader from "./src/components/Loader";
+
+export default () => {
+  const [loaded, setLoaded] = useState(false);
+
+  const preLoad = async () => {
+    try {
+      await Asset.loadAsync([require("./assets/icon.png")]);
+
+      setLoaded(true);
+    } catch (error) {
+      console.log("Error @preLoad_App: ", error.message);
+    }
+  };
+
+  useEffect(() => {
+    preLoad();
+  }, []);
+
+  return loaded ? (
+    <GameProvider>
+      <AppStack />
+    </GameProvider>
+  ) : (
+    <Loader />
+  );
+};
