89a094af1c4aceca6c9fc64e41cacfb570cf63e2
superbeom <it.super.beom@gmail.com>
2020-11-24 11:59:20 +0900

    Preloading Cache

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Asset } from "expo-asset";
+import AsyncStorage from "@react-native-community/async-storage";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { persistCache } from "apollo-cache-persist";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/client";
+
+import apolloClientOptions from "./apollo";
 
 import { GameProvider } from "./src/context/GameContext";
 import AppStack from "./src/stacks/AppStack";
@@ -8,12 +15,26 @@ import Loader from "./src/components/Loader";
 
 export default () => {
   const [loaded, setLoaded] = useState(false);
+  const [client, setClient] = useState(null);
 
   const preLoad = async () => {
     try {
+      /* Load Asset */
       await Asset.loadAsync([require("./assets/icon.png")]);
 
+      /* Load Cache */
+      const cache = new InMemoryCache();
+      await persistCache({
+        cache,
+        storage: AsyncStorage,
+      });
+      const apolloClient = new ApolloClient({
+        cache,
+        ...apolloClientOptions,
+      });
+
       setLoaded(true);
+      setClient(apolloClient);
     } catch (error) {
       console.log("Error @preLoad_App: ", error.message);
     }
@@ -23,10 +44,12 @@ export default () => {
     preLoad();
   }, []);
 
-  return loaded ? (
-    <GameProvider>
-      <AppStack />
-    </GameProvider>
+  return loaded && client ? (
+    <ApolloProvider client={client}>
+      <GameProvider>
+        <AppStack />
+      </GameProvider>
+    </ApolloProvider>
   ) : (
     <Loader />
   );
