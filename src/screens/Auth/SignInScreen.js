e11035d215581aa35dc3fccf763aaf115393822f
superbeom <it.super.beom@gmail.com>
2020-11-25 15:13:58 +0900

    Merge SignUp and SignIn to One Component Auth

--- src/screens/Auth/SignInScreen.js
+++ src/screens/Auth/SignInScreen.js
@@ -1,24 +1,13 @@
 import React, { useState } from "react";
-import {
-  StyleSheet,
-  View,
-  Text,
-  TouchableOpacity,
-  Alert,
-  ImageBackground,
-  TouchableWithoutFeedback,
-  Keyboard,
-} from "react-native";
+import { Alert } from "react-native";
 import { useMutation } from "react-apollo-hooks";
-import { vw, vh } from "react-native-expo-viewport-units";
 
 import { useLogIn } from "../../context/AuthContext";
 
 import { CONFIRM_SECRET } from "./AuthQueries";
 
-import colors from "../../constants/colors";
 import {
-  // SIGN_IN,
+  SIGN_IN,
   USERNAME,
   CANT_BE_EMPTY,
   INVALID_PASSWORD,
@@ -28,9 +17,7 @@ import {
 } from "../../constants/strings";
 
 import useInput from "../../hooks/useInput";
-import AuthInput from "../../components/AuthInput";
-import AuthButton from "../../components/AuthButton";
-// import Auth from "../../components/Auth";
+import Auth from "../../components/Auth";
 
 export default SignInScreen = ({ route, navigation }) => {
   const logIn = useLogIn();
@@ -81,97 +68,14 @@ export default SignInScreen = ({ route, navigation }) => {
     }
   };
 
-  // return (
-  //   <Auth
-  //     content={SIGN_IN}
-  //     handleAuth={handleSignIn}
-  //     usernameInput={usernameInput}
-  //     secretInput={secretInput}
-  //     loading={loading}
-  //     navigation={navigation}
-  //   />
-  // );
-
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <ImageBackground
-        style={styles.screen}
-        source={require("../../../assets/images/background.png")}
-      >
-        <View style={styles.header}>
-          <Text style={styles.headerText}>Welcome back!</Text>
-        </View>
-
-        <View style={styles.inputContainer}>
-          <View style={styles.inputBox}>
-            <Text style={styles.inputText}>Username</Text>
-            <AuthInput {...usernameInput} returnKeyType={"next"} />
-          </View>
-
-          <View style={styles.inputBox}>
-            <Text style={styles.inputText}>Password</Text>
-            <AuthInput
-              {...secretInput}
-              onSubmitEditing={handleSignIn}
-              secureTextEntry={true}
-            />
-          </View>
-        </View>
-
-        <AuthButton onPress={handleSignIn} text={"Log In"} loading={loading} />
-
-        <TouchableOpacity
-          onPress={() => navigation.navigate("SignUp")}
-          activeOpacity={0.5}
-        >
-          <Text style={styles.signUpLeftText}>
-            First time here?{"  "}
-            <Text style={styles.signUpRightText}>Sign Up</Text>{" "}
-          </Text>
-        </TouchableOpacity>
-      </ImageBackground>
-    </TouchableWithoutFeedback>
+    <Auth
+      content={SIGN_IN}
+      handleAuth={handleSignIn}
+      usernameInput={usernameInput}
+      secretInput={secretInput}
+      loading={loading}
+      navigation={navigation}
+    />
   );
 };
-
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-  },
-  header: {
-    marginTop: vh(15),
-    marginBottom: vh(15),
-  },
-  headerText: {
-    fontSize: vh(4),
-    fontWeight: "500",
-    textAlign: "center",
-    color: colors.whiteColor,
-  },
-  inputContainer: {
-    marginBottom: vh(15),
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  inputBox: {
-    width: "100%",
-    alignItems: "center",
-    marginBottom: vh(4),
-  },
-  inputText: {
-    width: "70%",
-    color: colors.whiteColor,
-    fontSize: vh(1.5),
-    textTransform: "uppercase",
-    fontWeight: "500",
-  },
-  signUpLeftText: {
-    fontSize: vw(4),
-    textAlign: "center",
-  },
-  signUpRightText: {
-    fontSize: vw(4.5),
-    fontWeight: "600",
-    color: colors.whiteColor,
-  },
-});
--- src/screens/Auth/SignUpScreen.js
+++ src/screens/Auth/SignUpScreen.js
@@ -1,23 +1,12 @@
 import React, { useState } from "react";
-import {
-  StyleSheet,
-  View,
-  Text,
-  TouchableWithoutFeedback,
-  Alert,
-  Keyboard,
-  TouchableOpacity,
-  ImageBackground,
-} from "react-native";
+import { Alert } from "react-native";
 import { useMutation } from "react-apollo-hooks";
-import { vw, vh } from "react-native-expo-viewport-units";
 
 import { CREATE_ACCOUNT } from "./AuthQueries";
 
 import { checkValueName } from "../../utils/checkAuthInput";
-import colors from "../../constants/colors";
 import {
-  // SIGN_UP,
+  SIGN_UP,
   EMPTY,
   BLANK,
   USERNAME,
@@ -34,9 +23,7 @@ import {
 } from "../../constants/strings";
 
 import useInput from "../../hooks/useInput";
-import AuthInput from "../../components/AuthInput";
-import AuthButton from "../../components/AuthButton";
-// import Auth from "../../components/Auth";
+import Auth from "../../components/Auth";
 
 export default ({ navigation }) => {
   const [loading, setLoading] = useState(false);
@@ -103,97 +90,14 @@ export default ({ navigation }) => {
     }
   };
 
-  // return (
-  //   <Auth
-  //     content={SIGN_UP}
-  //     handleAuth={handleSignup}
-  //     usernameInput={usernameInput}
-  //     secretInput={secretInput}
-  //     loading={loading}
-  //     navigation={navigation}
-  //   />
-  // );
-
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <ImageBackground
-        style={styles.screen}
-        source={require("../../../assets/images/background.png")}
-      >
-        <View style={styles.header}>
-          <Text style={styles.headerText}>Welcome!</Text>
-        </View>
-
-        <View style={styles.inputContainer}>
-          <View style={styles.inputBox}>
-            <Text style={styles.inputText}>Username</Text>
-            <AuthInput {...usernameInput} returnKeyType={"next"} />
-          </View>
-
-          <View style={styles.inputBox}>
-            <Text style={styles.inputText}>Password</Text>
-            <AuthInput
-              {...secretInput}
-              onSubmitEditing={handleSignup}
-              secureTextEntry={true}
-            />
-          </View>
-        </View>
-
-        <AuthButton onPress={handleSignup} text={"Sign Up"} loading={loading} />
-
-        <TouchableOpacity
-          onPress={() => navigation.navigate("SignIn")}
-          activeOpacity={0.5}
-        >
-          <Text style={styles.signInLeftText}>
-            Already have an account?{"  "}
-            <Text style={styles.signInRightText}>Log In</Text>{" "}
-          </Text>
-        </TouchableOpacity>
-      </ImageBackground>
-    </TouchableWithoutFeedback>
+    <Auth
+      content={SIGN_UP}
+      handleAuth={handleSignup}
+      usernameInput={usernameInput}
+      secretInput={secretInput}
+      loading={loading}
+      navigation={navigation}
+    />
   );
 };
-
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-  },
-  header: {
-    marginTop: vh(15),
-    marginBottom: vh(15),
-  },
-  headerText: {
-    fontSize: vh(4),
-    fontWeight: "500",
-    textAlign: "center",
-    color: colors.whiteColor,
-  },
-  inputContainer: {
-    marginBottom: vh(15),
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  inputBox: {
-    width: "100%",
-    alignItems: "center",
-    marginBottom: vh(4),
-  },
-  inputText: {
-    width: "70%",
-    color: colors.whiteColor,
-    fontSize: vh(1.5),
-    textTransform: "uppercase",
-    fontWeight: "500",
-  },
-  signInLeftText: {
-    fontSize: vw(4),
-    textAlign: "center",
-  },
-  signInRightText: {
-    fontSize: vw(4.5),
-    fontWeight: "600",
-    color: colors.whiteColor,
-  },
-});
