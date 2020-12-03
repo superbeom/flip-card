4723de70d132fc6ace556c867865236235bb80cc
superbeom <it.super.beom@gmail.com>
2020-12-03 21:53:08 +0900

    Update Game Stage part Four - until stage 400

--- src/utils/checkSomething.js
+++ src/utils/checkSomething.js
@@ -1,32 +1,49 @@
 /* stage에 따른, 정답 갯수 */
 export const checkAnswer = (stage) => {
-  if (stage === 1 || stage === 6 || stage === 37 || stage === 158) {
+  if (
+    stage === 1 ||
+    stage === 6 ||
+    stage === 37 ||
+    stage === 158 ||
+    stage === 335
+  ) {
     return 1;
   } else if (
     (stage > 1 && stage <= 5) ||
     (stage > 6 && stage <= 9) ||
     (stage > 22 && stage <= 26) ||
     (stage > 37 && stage <= 39) ||
-    stage === 159
+    stage === 159 ||
+    (stage > 335 && stage <= 340)
   ) {
     return 2;
   } else if (
     (stage > 9 && stage <= 14) ||
     (stage > 26 && stage <= 30) ||
     (stage > 39 && stage <= 43) ||
-    (stage > 159 && stage <= 163)
+    (stage > 159 && stage <= 163) ||
+    (stage > 340 && stage <= 350)
   ) {
     return 3;
   } else if (
     (stage > 14 && stage <= 22) ||
     (stage > 30 && stage <= 36) ||
     (stage > 43 && stage <= 60) ||
-    (stage > 163 && stage <= 167)
+    (stage > 163 && stage <= 167) ||
+    (stage > 350 && stage <= 366)
   ) {
     return 4;
-  } else if ((stage > 60 && stage <= 81) || (stage > 167 && stage <= 172)) {
+  } else if (
+    (stage > 60 && stage <= 81) ||
+    (stage > 167 && stage <= 172) ||
+    (stage > 366 && stage <= 386)
+  ) {
     return 5;
-  } else if ((stage > 81 && stage <= 99) || (stage > 172 && stage <= 186)) {
+  } else if (
+    (stage > 81 && stage <= 99) ||
+    (stage > 172 && stage <= 186) ||
+    (stage > 386 && stage <= 4000)
+  ) {
     return 6;
   } else if ((stage > 99 && stage <= 118) || (stage > 186 && stage <= 210)) {
     return 7;
@@ -38,7 +55,7 @@ export const checkAnswer = (stage) => {
     return 10;
   } else if (stage > 290 && stage <= 311) {
     return 11;
-  } else if (stage > 311 && stage <= 400000) {
+  } else if (stage > 311 && stage <= 334) {
     return 12;
   } else if (stage > 300000 && stage <= 400000) {
     return 13;
