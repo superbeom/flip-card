25d8ee629fe718d79d38a1a6fd1d581686345ac2
superbeom <it.super.beom@gmail.com>
2020-12-24 17:58:00 +0900

    Update Language packs part2 - Japanese

--- src/constants/strings.js
+++ src/constants/strings.js
@@ -1,14 +1,9 @@
 /*
 
+    Common Strings...
 
-
-    English Strings...
-
-
-    
 */
 
-// /* Auth */
 export const SIGN_UP = "SignUp";
 
 export const SIGN_IN = "SignIn";
@@ -17,6 +12,33 @@ export const SIGN_UP_TITLE = "Welcome!";
 
 export const SIGN_IN_TITLE = "Welcome back!";
 
+export const EMPTY = "Empty";
+
+export const BLANK = "Blank";
+
+export const HOME = "Home";
+
+export const RANK = "Rank";
+
+export const CONGRATULATIONS = "CONGRATULATIONS!!";
+
+export const COMING_SOON = "Coming soon!";
+
+export const COMING_SOON_CAPITAL = "COMING\nSOON!";
+
+export const QUESTION_MARK = "?";
+
+export const CURRENT_STAGE = "STAGE";
+
+export const GAME_START = "GAME\nSTART";
+
+/*
+
+    English Strings...
+
+*/
+
+// /* Auth */
 // export const SIGN_UP_TEXT = "Sign Up";
 
 // export const SIGN_IN_TEXT = "Log In";
@@ -24,10 +46,6 @@ export const SIGN_IN_TITLE = "Welcome back!";
 // export const HAVE_BEEN = "Already have an account?";
 
 // export const FIRST_TIME = "First time here?";
-
-export const EMPTY = "Empty";
-
-export const BLANK = "Blank";
 
 // export const USERNAME = "Username";
 
@@ -52,7 +70,6 @@ export const BLANK = "Blank";
 // export const USERNAME_ALREADY_TAKEN = "This username is already taken!";
 
 // /* Sign In */
-
 // export const DOESNT_EXIST = 'This username does not exist'
 
 // export const INVALID_PASSWORD = "Invalid password";
@@ -64,17 +81,7 @@ export const BLANK = "Blank";
 // export const CANT_CONFIRM_PASSWORD = "Can't confirm password";
 
 // /* AppStack Strings */
-export const HOME = "Home";
-
-export const RANK = "Rank";
-
-export const CONGRATULATIONS = "CONGRATULATIONS!!";
-
 // export const CLICK_TO_GET_HEARTS = "Click to get hearts";
-
-export const COMING_SOON = "Coming soon!";
-
-export const COMING_SOON_CAPITAL = "COMING\nSOON!";
 
 // export const HOLD_ON = "Hold on!";
 
@@ -83,13 +90,6 @@ export const COMING_SOON_CAPITAL = "COMING\nSOON!";
 // export const CANCEL = 'CANCEL'
 
 // export const EXIT = 'EXIT'
-
-export const QUESTION_MARK = "?";
-
-// /* StartGameScreen Strings */
-export const CURRENT_STAGE = "STAGE";
-
-export const GAME_START = "GAME\nSTART";
 
 // /* GameScreen Strings */
 // export const GO_HOME = "GO HOME";
@@ -103,73 +103,139 @@ export const GAME_START = "GAME\nSTART";
 
 /*
 
-
-
     Korean Strings...
 
+*/
 
+// /* Auth */
+// export const SIGN_UP_TEXT = "계정 생성";
+
+// export const SIGN_IN_TEXT = "로그인";
+
+// export const HAVE_BEEN = "계정을 가지고 있어요?";
+
+// export const FIRST_TIME = "처음이세요?";
+
+// export const USERNAME = "유저네임";
+
+// export const PASSWORD = "비밀번호";
+
+// export const CANT_BE_EMPTY = "을(를) 작성해 주세요!";
+
+// export const CANT_BE_BLANK = "을(를) 작성해 주세요!";
+
+// /* Sign Up */
+// export const ACCOUNT_CREATED = "계정이 생성되었습니다!";
+
+// export const LETS_PLAY_A_GAME = "즐겁게 게임을 즐기세요:)";
+
+// export const CANT_CREATE_ACCOUNT = "계정을 생성할 수 없습니다";
+
+// export const USERNAME_ONLY =
+//   "유저네임은 '문자 / 숫자 / 밑줄(_) / 마침표(.)'만 사용할 수 있어요!";
+
+// export const USERNAME_UNDER_20 = "유저네임을 20자 이하로 작성해 주세요!";
+
+// export const USERNAME_ALREADY_TAKEN = "이 유저네임은 이미 사용 중입니다!";
+
+// /* Sign In */
+// export const DOESNT_EXIST = "해당 유저네임은 존재하지 않습니다";
+
+// export const INVALID_PASSWORD = "유효하지 않은 비밀번호입니다";
+
+// export const WRONG_PASSWORD = "비밀번호가 일치하지 않습니다!";
+
+// export const CHECK_USERNAME_PASSWORD = "유저네임과 비밀번호를 확인해 주세요";
+
+// export const CANT_CONFIRM_PASSWORD = "비밀번호를 확인할 수 없습니다";
+
+// /* AppStack Strings */
+// export const CLICK_TO_GET_HEARTS = "하트 획득하러 가기";
+
+// export const HOLD_ON = "잠깐만요!";
+
+// export const CHECK_EXIT = "정말 게임을 종료하시겠습니까?";
+
+// export const CANCEL = "아니오";
+
+// export const EXIT = "종료하기";
+
+// /* GameScreen Strings */
+// export const GO_HOME = "홈 화면으로";
+
+// export const CHECK_GO_HOME = "홈 화면으로 가시겠습니까?";
+
+// /* GameOverScreen Strings */
+// export const PLAY_AGAIN = "다시\n하기";
+
+// export const NEXT_STAGE = "다음\n단계";
+
+/*
+
+    Japanese Strings...
 
 */
 
 /* Auth */
-export const SIGN_UP_TEXT = "계정 생성";
+export const SIGN_UP_TEXT = "アカウントの作成";
 
-export const SIGN_IN_TEXT = "로그인";
+export const SIGN_IN_TEXT = "ログイン";
 
-export const HAVE_BEEN = "계정을 가지고 있어요?";
+export const HAVE_BEEN = "既にアカウントをお持ちですか?";
 
-export const FIRST_TIME = "처음이세요?";
+export const FIRST_TIME = "こちらは初めてですか？";
 
-export const USERNAME = "유저네임";
+export const USERNAME = "ユーザー名";
 
-export const PASSWORD = "비밀번호";
+export const PASSWORD = "パスワード";
 
-export const CANT_BE_EMPTY = "을(를) 작성해 주세요!";
+export const CANT_BE_EMPTY = "を作成してください!";
 
-export const CANT_BE_BLANK = "을(를) 작성해 주세요!";
+export const CANT_BE_BLANK = "を作成してください!";
 
 /* Sign Up */
-export const ACCOUNT_CREATED = "계정이 생성되었습니다!";
+export const ACCOUNT_CREATED = "アカウントが作成されました!";
 
-export const LETS_PLAY_A_GAME = "즐겁게 게임을 즐기세요:)";
+export const LETS_PLAY_A_GAME = "楽しくゲームをお楽しみください:)";
 
-export const CANT_CREATE_ACCOUNT = "계정을 생성할 수 없습니다";
+export const CANT_CREATE_ACCOUNT = "アカウントを作成できません";
 
 export const USERNAME_ONLY =
-  '유저네임은 "문자 / 숫자 / 밑줄(_) / 마침표(.)"만 사용할 수 있어요!';
+  "ユーザー名に使用できるのは、文字、数字、下線、ピリオドだけです。";
 
-export const USERNAME_UNDER_20 = "유저네임을 20자 이하로 작성해 주세요!";
+export const USERNAME_UNDER_20 = "ユーザー名を20文字以下で入力してください!";
 
-export const USERNAME_ALREADY_TAKEN = "이 유저네임은 이미 사용 중입니다!";
+export const USERNAME_ALREADY_TAKEN = "このユーザー名は既に使用中です!";
 
 /* Sign In */
-export const DOESNT_EXIST = "해당 유저네임은 존재하지 않습니다";
+export const DOESNT_EXIST = "このユーザー名は存在しません。";
 
-export const INVALID_PASSWORD = "유효하지 않은 비밀번호입니다";
+export const INVALID_PASSWORD = "無効なパスワードです";
 
-export const WRONG_PASSWORD = "비밀번호가 일치하지 않습니다!";
+export const WRONG_PASSWORD = "パスワードが一致しません!";
 
-export const CHECK_USERNAME_PASSWORD = "유저네임과 비밀번호를 확인해 주세요";
+export const CHECK_USERNAME_PASSWORD =
+  "ユーザー名とパスワードを確認してください";
 
-export const CANT_CONFIRM_PASSWORD = "비밀번호를 확인할 수 없습니다";
+export const CANT_CONFIRM_PASSWORD = "パスワードを確認できません";
 
 /* AppStack Strings */
-export const CLICK_TO_GET_HEARTS = "하트 획득하러 가기";
+export const CLICK_TO_GET_HEARTS = "ハート獲得する";
 
-export const HOLD_ON = "잠깐만요!";
+export const HOLD_ON = "ちょっと待って!";
 
-export const CHECK_EXIT = "정말 게임을 종료하시겠습니까?";
+export const CHECK_EXIT = "本当にゲームを終了しますか？";
 
-export const CANCEL = "아니오";
+export const CANCEL = "いいえ";
 
-export const EXIT = "종료하기";
+export const EXIT = "終了する";
 
 /* GameScreen Strings */
-export const GO_HOME = "홈 화면으로";
+export const GO_HOME = "ホーム画面に";
 
-export const CHECK_GO_HOME = "홈 화면으로 가시겠습니까?";
+export const CHECK_GO_HOME = "ホーム画面に戻りますか?";
 
 /* GameOverScreen Strings */
-export const PLAY_AGAIN = "다시\n하기";
+export const PLAY_AGAIN = "再び\nプレー";
 
-export const NEXT_STAGE = "다음\n단계";
+export const NEXT_STAGE = "次の\n段階";
